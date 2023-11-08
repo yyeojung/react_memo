7a9c70b6bfb210f13d8cc00ad8d97fe6966393ed
hongyeojung <yeojung@uxstory.co.kr>
2023-11-08 18:04:59 +0900

    height 수정

--- src/components/Notepad.js
+++ src/components/Notepad.js
@@ -17,6 +17,9 @@ const MemoWrap = styled.div`
     top:50%;
     left: 50%;
     transform:translate(-50%, -50%);
+    @media all and (max-width: 500px) {
+        height:calc(100vh - 4rem);
+    }
 `;
 const AddBtnWrap = styled.div`
     margin:2rem 1.6rem 1rem 0;
